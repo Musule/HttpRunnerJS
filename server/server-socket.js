0f3ee87e7be14f8409db3fb822c93e5b1c0b90d9
李银池 <yinchi.li@ucarinc.com>
2020-06-13 19:46:52 +0800

    广播事件

--- server/server-socket.js
+++ server/server-socket.js
@@ -1,18 +1,18 @@
 var fs = require('fs');
-var chalk=require('chalk');
+var chalk = require('chalk');
 const server = require('http').createServer();
 const io = require('socket.io')(server);
-const port=4004;
+const port = 4004;
 const url = "127.0.0.1"
 io.on('connection', client => {
 
   // 服务端绑定event事件
-  client.on('Client2Server', data => { 
+  client.on('Client2Server', data => {
     console.log(chalk.blue.bold(`【Server端】接收到数据内容：${data}`));
   });
 
   // 服务端绑定event事件
-  client.on('disconnect', () => { 
+  client.on('disconnect', () => {
     console.log(chalk.blue.red(`已断开连接`));
   });
 
@@ -21,10 +21,12 @@ io.on('connection', client => {
     console.log(chalk.blue.bold(`【Server端】发生错误： ${error}`));
   });
   // 服务端触发客户端event事件
-  client.emit('Server2Client','我是服务端的数据，处理完数据我传给了客户端')
+  client.emit('Server2Client', '我是服务端的数据，处理完数据我传给了客户端');
+  // 广播时间
+  client.broadcast('broadevent', ' 这是一条广播消息，除了发送者其他人都可以接收到');
 });
 
-  // 服务端监听地址和端口
+// 服务端监听地址和端口
 server.listen(port, url);
 
 console.log(chalk.yellow.bold(`Server running at http://${url}:${port}`));
--- test/client-socket.js
+++ test/client-socket.js
@@ -5,24 +5,26 @@ describe('socket client', () => {
         var socket = require('socket.io-client')('http://127.0.0.1:4004/');
         // 发生连接时
         socket.on('connect', () => {
-            console.log(chalk.blue.bold("【Client端】Connect Sever Success"))
+            console.log(chalk.blue.bold("【Client端3】Connect Sever Success"))
         });
         // 客户端触发服务端event事件
-        socket.emit('Client2Server', '这是发送给服务端的数据111111', () => {// 触发事件
-            console.log(chalk.blue.bold("【Client端】客户端把数据发给服务端"));
+        socket.emit('Client2Server', '【Client端3】发送给服务端的数据：你好服务器11111', () => {// 触发事件
+            console.log(chalk.blue.bold("【Client端3】客户端把数据发给服务端"));
         });
         // 客户端触发服务端event事件
-        socket.emit('Client2Server', '这是发送给服务端的数据222222', () => {// 触发事件
-            console.log(chalk.blue.bold("【Client端】客户端把数据发给服务端"));
+        socket.emit('Client2Server', '【Client端3】发送给服务端的数据：你好服务器22222', () => {// 触发事件
+            console.log(chalk.blue.bold("【Client端3】客户端把数据发给服务端"));
         });
         // 绑定event事件
         socket.on('Server2Client', (data) => {// 绑定事件
-            console.log(chalk.blue.bold(`【Client端】服务端把处理后的数据，传递给客户端，内容是：${data}`));
+            console.log(chalk.blue.bold(`【Client端3】服务端把处理后的数据，传递给客户端，内容是：${data}`));
         });
         // 发生断开连接时
         socket.on('disconnect', () => {
-            console.log(chalk.blue.bold(`disconnect`));
+            console.log(chalk.blue.bold(`【Client端3】disconnect`));
         });
+       
+    
     })
 
 });
